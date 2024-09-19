113a462c84c7a99fd2258a7d28ed8c45fdf7fe1c
Mohammad Wahid <mohammadwahid.eng@gmail.com>
2024-09-19 16:24:37 +0600

    jest initialize

---
+++ src/__tests__/e2e/text-analyzer.spec.ts
@@ -0,0 +1,21 @@
+describe('Text Analyzer API Test', () => {
+  describe('GET /count/words', () => {
+    test.todo('should return the number of words');
+  });
+
+  describe('GET /count/characters', () => {
+    test.todo('should return the number of characters');
+  });
+
+  describe('GET /count/sentences', () => {
+    test.todo('should return the number of sentences');
+  });
+
+  describe('GET /count/paragraphs', () => {
+    test.todo('should return the number of paragraphs');
+  });
+
+  describe('GET /longest-words', () => {
+    test.todo('should return the longest words in paragraphs');
+  });
+});
