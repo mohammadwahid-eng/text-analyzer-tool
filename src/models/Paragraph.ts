766f81f6e98950ccde16031e82c820f31aac768c
Mohammad Wahid <mohammadwahid.eng@gmail.com>
2024-09-19 22:40:21 +0600

    create paragraph added

--- src/models/Paragraph.ts
+++ src/models/Paragraph.ts
@@ -7,16 +7,33 @@ const paragraphSchema = new Schema<IParagraph>({
   totalCharacters: { type: Number, default: 0 },
   totalSentences: { type: Number, default: 0 },
   totalParagraphs: { type: Number, default: 0 },
-  longestWords: [String]
+  longestWords: { type: [String], default: [] },
 }, { timestamps: true });
 
 paragraphSchema.pre('save', function(next) {
   // ignore punctuation
+  const content = this.body.replace(/[,.\/?:\\;"_'{}[\]()—*&^%$#@!~`]/g, ''); // Punctuation: ,./?:\;"_'{}[]()—*&^%$#@!~`
+  
   // count total words
+  this.totalWords = content.split(/\s+/).filter(word => word !== '').length;
+  
   // count total characters
+  this.totalCharacters = content.length;
+
   // count total sentences
+  const contentFilter = this.body.replace(/[,\/:\\;"_'{}[\]()—*&^%$#@~`]/g, ''); // Punctuation removed except .!?
+  this.totalSentences = contentFilter.split(/[.?!]+/).filter(sentence => sentence !== '').length;
+  
   // count total paragraphs
+  this.totalParagraphs = content.split(/\n+/).filter(paragraph => paragraph !== '').length;
+  
   // set longest words
+  const getWords     = content.split(/\s+/);
+  const lengths      = getWords.map(word => word.length);
+  const maxLength    = Math.max(...lengths);
+  const longestWords = getWords.filter(item => item.length === maxLength);
+  this.longestWords  = this.longestWords ? this.longestWords.concat(longestWords) : [];
+  
   next();
 });
 
